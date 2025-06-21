10556ef9617c8ee7029a7f70fd2b495215ef0d52
SyoinGit <sasasyuga@yahooo.co.jp>
2025-06-21 21:15:18 +0900

    プロジェクト初期構成（controller/data/service ディレクトリ作成）

---
+++ src/main/java/com/tftforge/TftforgeApplication.java
@@ -0,0 +1,13 @@
+package com.tftforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tftforge/TftforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tftforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
